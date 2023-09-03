0b670ce24bfdc13d6531d71cf547b38dad39b6da
Kelvin Pedroso <vieirapedroso@gmail.com>
2023-09-02 22:42:48 -0300

    base e scripts adicionados

---
+++ training.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
+from sklearn import svm
+from sklearn import tree
+from sklearn.svm import SVR
+from sklearn.svm import LinearSVR
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Lasso
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import RandomizedSearchCV
+from scipy.stats import uniform as sp_randFloat
+from scipy.stats import randint as sp_randInt
+from sklearn.model_selection import StratifiedShuffleSplit
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from sklearn.tree import export_graphviz
+from statistics import mean
+from collections import Counter
+from sklearn.datasets import make_classification
+from imblearn.over_sampling import RandomOverSampler
+import statsmodels.api as sm
+from sklearn.metrics import accuracy_score
+
+
+data_path = "~/Documentos/github/kaggle_store_sales/"
+train = data_path + 'holidays_events.csv'               
+
+dataset = pd.read_csv(train)
